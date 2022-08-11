c4e081986e2f83a8e63d2b354784f6b25ebd5448
vamC <108390032+vamshiss@users.noreply.github.com>
2022-08-11 20:52:02 +0530

    tm
    tm

---
+++ textbox.js
@@ -0,0 +1,13 @@
+
+    <script>
+        
+
+    function validate(){
+        fname = document.getElementById(fname)
+        lname = document.getElementById(lname)
+        address = document.getElementById(add)
+        email = document.getElementById(email)
+        if (fname==""){
+            alert("empty not allowed")
+        }
+    }
